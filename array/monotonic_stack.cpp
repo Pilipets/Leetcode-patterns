b99a967e1258ae62bc39381de9bcbc3e1d43a101
Hlib Pylypets <pilipets.gleb@gmail.com>
2024-12-19 23:23:37 -0500

    Update monotonic_stack.cpp

--- array/monotonic_stack.cpp
+++ array/monotonic_stack.cpp
@@ -24,3 +24,34 @@ int fn(vector<int>& arr)
         stack.push(num);
     }
 }
+
+
+// https://leetcode.com/problems/number-of-valid-subarrays/solution/
+//
+class Solution {
+public:
+    int validSubarrays(vector<int>& nums) {
+        int res = 0;
+        stack<int> st;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            while (!st.empty() && nums[i] < nums[st.top()])
+            {
+                // pop all items smaller than current
+                res += (i - st.top());
+                st.pop();
+            }
+            
+            st.push(i);
+        }
+        
+        while (!st.empty())
+        {
+            res += (nums.size() - st.top());
+            st.pop();
+        }
+        
+        return res;
+    }
+};
